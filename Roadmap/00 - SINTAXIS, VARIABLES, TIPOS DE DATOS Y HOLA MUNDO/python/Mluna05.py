d68e28ad392ae8bb0aad9dc4eaf4bb0669bc4492
nemesis.gun.05 <nemesis.gun.05@gmail.com>
2024-06-23 02:41:18 +0000

    00_Sintaxis_HolaMundo

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Mluna05.py
@@ -0,0 +1,26 @@
+# https://www.python.org/
+
+"""
+Este es un comentario en varias lineas
+"""
+
+'''
+Esta es otra forma de escribir un comentario en varias lineas
+'''
+
+mi_variable = 10
+
+MY_COST = "no es una constante real"
+
+entero = 5
+caracteres = "python"
+booleano = True
+flotante = 3.55
+
+complejos = 3 + 5j
+complejo2 = 2 + 1j
+
+print(complejos.real)
+print(complejos.imag)
+
+print("¡Hola, Python!")
